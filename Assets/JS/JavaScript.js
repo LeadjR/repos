95cc69b3d0a24ee4a1a57a9661508899e7ecb8aa
Leadj <leadj@protonmail.com>
2022-06-24 12:12:43 +0300

    Added Projects

--- Assets/JS/JavaScript.js
+++ Assets/JS/JavaScript.js
@@ -111,3 +111,16 @@ const init = () => {
 $(() => {
 	init();
 });	
+
+var swiper = new Swiper('.blog-slider', {
+	effect: 'fade',
+	loop: true,
+	mousewheel: {
+        invert: false,
+	},
+	autoHeight: true,
+	pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+	}
+});
